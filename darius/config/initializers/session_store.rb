5f975fd8c6b33451b85ee70b8e72dc5d56408c78
Stephen Zhang <stephen.zsy@gmail.com>
2013-09-20 01:32:52 -0700

    initialized rails app darius

---
+++ darius/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Darius::Application.config.session_store :cookie_store, key: '_darius_session'
---
+++ darius/thrift/article_manager.thrift
@@ -0,0 +1,72 @@
+namespace java com.coldblossom.darius
+namespace rb ColdBlossom.Darius
+
+const i32 MAJOR_VERSION = 1
+const i32 MINOR_VERSION = 0
+const i32 PATCH_VERSION = 0
+
+enum StatusCode {
+  UNKNOWN,
+  SUCCESS,
+  ERROR, // when user input is invalid
+  FAULT, // when server encouters a error
+  SCHEDULED, // when requested resource is scheduled
+  UNAVAILABLE // when requested resource is unavailable
+}
+
+enum OutputType {
+  S3_ARN,
+  JSON,
+  TEXT
+}
+
+enum DocumentType {
+  ARTICLE,
+  DAILY_ARCHIVE_INDEX,
+  RSS_FEED
+}
+
+enum SchedulingOption {
+  DEFAULT, // will attempt to get from cache, if not, schedule to get the requested resource
+  NONE, // will not atttempt to schedule to get the requested resource and fail the call directly
+  IMMEDIATELY // synchronized call
+}
+
+enum CacheOption {
+  DEFAULT, // will attempt to get from cache if cache did not expire by default criteria
+  NO_CACHE, // will not attempt to get a cached copy or refresh the cache
+  ONLY_CACHE, // will only attempt cache without refresh
+  REFRESH // force refresh the cache
+}
+
+struct GetOriginalDocumentRequest {
+  1: string vendor,
+  2: DocumentType documentType,
+  3: string documentUrl,
+  4: string datetime,
+  5: OutputType outputType,
+  6: SchedulingOption schedulingOption,
+  7: CacheOption cacheOption
+}
+
+struct GetOriginalDocumentResult {
+  1: StatusCode statusCode,
+  2: string timestamp,
+  3: string document
+}
+
+exception ServiceException {
+  1: StatusCode statusCode,
+  2: string message
+}
+
+service ArticleManager {
+
+  string health()
+
+  string version()
+
+  GetOriginalDocumentResult getOriginalDocument(1: GetOriginalDocumentRequest request) throws (1: ServiceException e)
+
+}
+
