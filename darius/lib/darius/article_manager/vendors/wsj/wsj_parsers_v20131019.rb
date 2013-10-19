ba7ebf95f7754af84cba557e7b88060443324f25
Stephen Zhang <stephen.zsy@gmail.com>
2013-10-19 00:11:55 -0700

    parsers module for new website version

---
+++ darius/lib/darius/article_manager/vendors/wsj/wsj_parsers_v20131019.rb
@@ -0,0 +1,12 @@
+module ColdBlossom
+  module Darius
+    module ArticleManager
+      module VendorParsers
+        module WSJ
+          module V20131019
+          end
+        end
+      end
+    end
+  end
+end
